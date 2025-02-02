4441e00ceed85f4dbdde6ed153e290784c073c76
kanato3256 <c0b231359f@edu.teu.ac.jp>
2025-02-02 15:06:57 +0900

    mainファイルの追加

---
+++ main.py
@@ -0,0 +1,14 @@
+import csv
+
+def read_csv(file_path):
+    with open(file_path, mode='r', encoding='utf-8') as file:
+        csv_reader = csv.reader(file)
+        header = next(csv_reader)
+        data = [row for row in csv_reader]
+    return header, data
+
+if __name__ == "__main__":
+    file_path = 'data_file/test.csv'
+    header, data = read_csv(file_path)
+    print("Header:", header)
+    print("Data:", data)
