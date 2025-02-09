233ecb922d7445ed301053b656e8e817c4844f08
kanato3256 <c0b231359f@edu.teu.ac.jp>
2025-02-09 18:57:25 +0900

    csvの処理を追記

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import csv
-
+from module.player_data import PlayerData, get_possible_events, COMMON_EVENTS, FREESTYLE_EVENTS # 追加
 
 def read_csv(file_path):
     with open(file_path, mode="r", encoding="utf-8") as file:
@@ -8,5 +8,15 @@ def read_csv(file_path):
         data = [row for row in csv_reader]
     return header, data
 
+
 header, data  = read_csv("data_file/test.csv")
 print(data[0])
+pl_lst = []
+
+for n, i in enumerate(data):
+    if n == 0:
+        continue
+    else:
+        tmp = PlayerData(id=i[0], name=i[1], hurigana=i[2], team=i[3], grade=i[4], sex=i[5])
+        pl_lst.append(tmp)
+        print(tmp.name)
